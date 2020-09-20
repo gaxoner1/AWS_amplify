da0d530e45758fe0d16968eebdc29dad1d451975
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-09-20 02:40:39 -0400

    Add AWS AUTH

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import * as serviceWorker from './serviceWorker';
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
